747cf6f922b003ca7fe88db3985e6555e9b556ec
putrapantura <adsenekuala@gmail.com>
2022-08-03 09:17:14 +0700

    Membuat component pertama

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,21 @@
 <body>
     <h1>Home</h1>
     <h4>Belajar laravel 9</h4>
+
+
+    {{-- Components --}}
+
+    @component('components.alert')
+        @slot('header')
+            Header
+        @endslot
+        Hello ini adalah components
+
+        @slot('footer')
+            Footer
+        @endslot
+    @endcomponent
+
 </body>
 
 </html>
